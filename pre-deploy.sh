6bdaf2ffcdbcb4ba8a607cfd991e335a8d28151f
Rahul Salvi <rsalvi006@gmail.com>
2025-08-10 15:01:06 -0700

    add compose file and pre-deploy script

---
+++ pre-deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# return code
+RC=0
+
+git submodule update --init --recursive
+
+if [ ! -e .env ]; then
+    touch .env
+fi
+chmod -f 600 .env
+
+if ! grep -q "TS_AUTHKEY" .env; then
+    if [ -z "$TS_API_CLIENT_ID" ]; then
+        echo "ERROR: no tailscale oauth credentials provided"
+        RC=1
+    else
+        echo "Adding TS_AUTHKEY to .env"
+        echo "TS_AUTHKEY=$(get-authkey -tags tag:webserver -ephemeral -preauth)" >>.env
+    fi
+fi
+
+if ! grep -q "INFISICAL" .env; then
+    if [ -z "$INFISICAL_TOKEN" ]; then
+        echo "ERROR: no infisical token provided"
+        RC=1
+    else
+        echo "Adding secrets from infisical to .env"
+        {
+            echo "# BEGIN INFISICAL SECRETS"
+            infisical export --env prod --path /karakeep
+            echo "# END INFISICAL SECRETS"
+        } >>.env
+    fi
+fi
+
+exit $RC
